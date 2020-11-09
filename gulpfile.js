d885666a244da805793d059dfaaf38a29c5f5809
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-11-09 23:39:19 +0200

    update gulpfile default

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-// compile scss into css
 function style() {
-    // 1/ where is my scss file
     return (
         gulp
             .src('./scss/**/*.scss')
-            // 2. pass that file through sass compiler
             .pipe(sass().on('error', sass.logError))
-            // 3. where do I save the compiled CSS?
             .pipe(gulp.dest('./css'))
-            // 4. stream changes to all browsers
             .pipe(browserSync.stream())
     );
 }
@@ -23,10 +18,11 @@ function watch() {
             baseDir: './',
         },
     });
+
     gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
-exports.watch = watch;
+exports.default = watch;
